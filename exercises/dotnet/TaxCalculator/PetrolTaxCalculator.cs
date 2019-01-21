dd5ead89eefacf329d2afb76a6d3a7825b829a5b
Brad Davies <brad_davies@waters.com>
2019-01-21 11:53:10 +0000

    All Petrol Tests Pass

--- exercises/dotnet/TaxCalculator/PetrolTaxCalculator.cs
+++ exercises/dotnet/TaxCalculator/PetrolTaxCalculator.cs
@@ -6,7 +6,51 @@ namespace TaxCalculator
     {
         public override int CalculateTax(Vehicle vehicle)
         {
-            return 0;
+            int[] emissionThresholds =
+            {
+                50,
+                75,
+                90,
+                100,
+                110,
+                130,
+                150,
+                170,
+                190,
+                225,
+                255
+            };
+
+            int[] taxAmounts =
+            {
+                10,
+                25,
+                105,
+                125,
+                145,
+                165,
+                205,
+                515,
+                830,
+                1240,
+                1760
+            };
+
+            if (vehicle.Co2Emissions == 0)
+            {
+                return 0;
+            }
+
+            for (var i = 0; i < emissionThresholds.Length; i++)
+            {
+                
+                if (vehicle.Co2Emissions <= emissionThresholds[i])
+                {
+                    return taxAmounts[i];
+                }
+            }
+
+            return 2070;
         }
     }
 }
